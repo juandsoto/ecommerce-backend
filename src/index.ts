7fae11ec0a66b118ee8dc4cfc7ea3501cfa14272
juandsoto <juandsoto1006@gmail.com>
2022-03-03 17:34:05 -0500

    Update: json, morgan and endpoint api prefix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 import config from "./config";
 import "./db/connection";
+import routes from "./pages";
 
 const app = express();
 
-app.use(express.json());
 app.set("PORT", config.PORT || 3001);
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api/v1", routes);
 
 app.listen(app.get("PORT"), () => {
   console.log(`server listening on port ${app.get("PORT")}`);
