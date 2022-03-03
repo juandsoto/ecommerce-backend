e1691ab8eb20fded76a94eca81c093820f8fce65
juandsoto <juandsoto1006@gmail.com>
2022-03-03 17:35:06 -0500

    Add: Errors and exception handling

---
+++ src/utils/errors/badRequest.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+
+export function badRequest(res: Response, error: any) {
+  if (error.name === "ValidationError") {
+    let errors: Record<string, string> = {};
+
+    Object.keys(error.errors).forEach(key => {
+      errors[key] = error.errors[key].message;
+    });
+
+    return res.status(400).json({ error: errors });
+  }
+
+  if (error.name === "MongoServerError") {
+    let errors: Record<string, string> = {};
+
+    Object.keys(error.keyValue).forEach(key => {
+      errors[key] = `${error.keyValue[key]} is already registered`;
+    });
+
+    return res.status(400).json({ error: errors });
+  }
+  return res.status(400).json({
+    error: error.message,
+  });
+}
