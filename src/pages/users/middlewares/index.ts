66f2f08edce9ce707e00c3cdf2cb598481824db2
juandsoto <juandsoto1006@gmail.com>
2022-03-06 18:18:35 -0500

    Change: middleware for forbidden DTO values

--- src/pages/users/middlewares/index.ts
+++ src/pages/users/middlewares/index.ts
@@ -1,18 +1,26 @@
-import { genSaltSync, hashSync } from "bcrypt";
 import { NextFunction, Request, Response } from "express";
+import { badRequest } from "../../../utils/errors/badRequest";
 
-export function hashPasswordMiddleware(req: Request, res: Response, next: NextFunction) {
-  const password = req.body.password;
-  if (password) {
-    try {
-      const salt = genSaltSync();
-      req.body.password = hashSync(password, salt);
-    } catch (error: any) {
-      return res.status(500).json({
-        error: error.name,
-        message: error.message,
-      });
-    }
+export function validateForbidden(req: Request, res: Response, next: NextFunction) {
+  const { email, is_admin } = req.body;
+  let errors: Record<string, string> = {};
+  switch (req.method) {
+    case "POST":
+      if (is_admin) errors["is_admin"] = "is_admin can not be assigned to a value";
+      break;
+    case "PATCH":
+      if (is_admin) errors["is_admin"] = "is_admin can not be assigned to a value";
+      if (email) errors["email"] = "email can not be changed";
+      break;
   }
+
+  if (Object.keys(errors).length !== 0) {
+    badRequest(res, {
+      name: "Forbidden payload",
+      errors,
+    });
+    return;
+  }
+
   next();
 }
